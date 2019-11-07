e1897958f8f824caf0a79d1aa5b9e7d08206526a
Oshan Mudannayake <oshan.ivantha@gmail.com>
2019-11-08 00:46:55 +0530

    Update base project

--- app/src/main/java/com/ivantha/androidocr/MainActivity.java
+++ app/src/main/java/com/ivantha/androidocr/MainActivity.java
@@ -2,7 +2,10 @@ package com.ivantha.androidocr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    private String extractText(Bitmap bitmap) throws Exception{
+        TessBaseAPI tessBaseApi = new TessBaseAPI();
+        tessBaseApi.init(DATA_PATH, "eng");
+        tessBaseApi.setImage(bitmap);
+        String extractedText = tessBaseApi.getUTF8Text();
+        tessBaseApi.end();
+        return extractedText;
+    }
+
 }
